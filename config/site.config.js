34acdbe52f92b8ce2b89c48856cca5f960ada1d4
Howard <46689708+hcao10@users.noreply.github.com>
2023-03-22 17:05:44 -0500

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/📁Temporary Folder/Temp Files', '/📁Temporary Folder/other','/📁Temporary Folder/movie','/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/📁Temporary Folder/Temp Files', '/📁Temporary Folder/other','/📁Temporary Folder/test','/📁Temporary Folder/movie','/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
